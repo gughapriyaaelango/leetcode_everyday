130aafa61d4950df413d608c3b4cab77e449e63f
Gughapriyaa Elango <gughapriyaaelango@gmail.com>
2025-03-03 19:05:23 -0600

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -38,3 +38,21 @@ class Solution:
                 result.append(False)
         return result
                 
+'''
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+'''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right],nums[left] = nums[left],nums[right]
+                left += 1
+
+
+        
