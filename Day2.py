04e85b68210089ef224667526faf519632dfc2ec
Gughapriyaa Elango <gughapriyaaelango@gmail.com>
2025-03-04 00:43:08 +0000

    Day 2

---
+++ Day2.py
@@ -0,0 +1,21 @@
+'''
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+'''
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1, len2 = len(str1), len(str2)
+        result = []
+        if str1+str2 != str2+str1:
+            return ""
+        def gcd(len1, len2):
+            min_val = min(len1,len2)
+            for i in range(min_val,0,-1):
+                if len1 % i == 0 and len2 % i ==0:
+                    return i
+            return 1
+        return str1[:gcd(len(str1),len(str2))]
+                
